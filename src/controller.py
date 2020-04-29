848b637180a4ba9089e957d06d537c1f8e61326d
amarch <aamarchuk@gmail.com>
2020-04-29 14:58:13 +0300

    update dockerfile to use exact alpine image + /predict in REST

--- src/controller.py
+++ src/controller.py
@@ -11,6 +11,20 @@ def postSomeThing():
     logger.info('name: %s',name)
     return "Hello %s" %name
 
+@app.route('/predict', methods=['POST'])
+def predict_pf():
+    content = request.json
+    model = content['model']
+    logger.info('model: %s',model)
+    
+    prices = content['prices']
+    logger.info('prices: %s',prices)
+
+    prev_pf = content['prev_pf']
+    logger.info('prev_pf: %s',prev_pf)
+
+    return "[0.1, 0, 0, 0.3, 0.6]"
+
 @app.route("/")
 def hello():
     return "Hello World!"
